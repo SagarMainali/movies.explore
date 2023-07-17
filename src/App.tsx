f2906ff55304fa9c3bdd1f6c4290d5131c31384a
Sagar Mainali <sagarmainali78@gmail.com>
2023-07-17 10:30:29 +0545

    updated tailwind.config.js file by adding custom colors and screens sizes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Tvshows } from './Pages/Tvshows'
 export function App() {
 
   return (
-    <div className="app">
+    <div className="app max-w-[1500px] mx-auto max-2xl:px-3 text-slate-300">
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-dark': '#001C30',
+        'logo-inherit': '#0FB6DF',
+      },
+      screens: {
+        'xsm': '500px',
+        'sm': '600px',
+        'md': '760px',
+        'lg': '1040px',
+        'xl': '1300px',
+        '2xl': '1500px',
+      },
+    },
   },
   plugins: [],
 }
