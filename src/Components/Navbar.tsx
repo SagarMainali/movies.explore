e96f91dc7e07382d2510e772ec63e82ae6b5aede
Sagar Mainali <sagarmainali78@gmail.com>
2023-07-19 17:05:45 +0545

    finalize navbar design

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -38,13 +38,15 @@ export function Navbar() {
                     <div className="flex items-center gap-3">
                          {/* Routes */}
                          <ul className="flex font-medium gap-1">
-                              <NavLink to='/movies' className={({ isActive }) => 'px-2 py-[2px] rounded-md ' + (isActive ? 'active-link' : '')}>Movies</NavLink>
-                              <NavLink to='/tvshows' className={({ isActive }) => 'px-2 py-[2px] rounded-md ' + (isActive ? 'active-link' : '')}>TV Shows</NavLink>
+                              <NavLink to='/movies'
+                                   className={({ isActive }) => 'px-2 py-[2px] rounded-md hover:bg-logo-inherit duration-300 hover:text-primary-dark ' + (isActive ? 'active-link' : '')}>Movies</NavLink>
+                              <NavLink to='/tvshows'
+                                   className={({ isActive }) => 'px-2 py-[2px] rounded-md hover:bg-logo-inherit duration-300 hover:text-primary-dark ' + (isActive ? 'active-link' : '')}>TV Shows</NavLink>
                          </ul>
                          {/* Search */}
-                         <div className="rounded-lg sm:w-[16rem] w-[12rem] overflow-hidden flex">
-                              <input type="text" placeholder="Search..." className="w-[87%] h-[32px] bg-slate-300 text-slate-900 text-sm font-medium caret-slate-900 outline-0 px-4 placeholder:text-slate-900 placeholder:text-sm" />
-                              <button className="w-[13%] h-[32px] bg-primary-dark/40" onClick={() => console.log('clicked')}>
+                         <div className="rounded-lg md:w-[16rem] w-[12rem] overflow-hidden flex">
+                              <input type="text" placeholder="Search..." className="w-[87%] h-[30px] bg-slate-300 text-slate-900 text-sm font-medium caret-slate-900 outline-0 px-4 placeholder:text-slate-900 placeholder:text-sm" />
+                              <button className="w-[13%] h-[30px] bg-primary-dark/40" onClick={() => console.log('clicked')}>
                                    <svg className="w-full md:h-[1.2rem] h-[1rem]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#0FB6DF">
                                         <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                                    </svg>
