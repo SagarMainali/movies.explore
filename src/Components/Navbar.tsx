d13fcac10c935eeae7c1b54d05728e8798c921c8
Sagar Mainali <sagarmainali78@gmail.com>
2023-07-26 23:28:01 +0545

    handling navigation and searchQuery for movies search

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,19 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import '../styles/navbar.css'
 import { useGlobalContext } from '../state_management/context'
+import { ChangeEvent, useState } from "react"
 
 export function Navbar() {
 
      const { menuTogglerActive, changeMenuTogglerState } = useGlobalContext()
+
+     const [searchQuery, setSearchQuery] = useState('')
+
+     function handleChange(event: ChangeEvent<HTMLInputElement>) {
+          setSearchQuery(event.target.value)
+     }
+
+     const navigate = useNavigate()
 
      return (
           // max-2xl:w-[calc(100%-24px)]
@@ -29,8 +38,8 @@ export function Navbar() {
 
                          {/* Search */}
                          <div className="rounded-lg sm:w-[14rem] w-full overflow-hidden flex">
-                              <input type="text" placeholder="Search..." className="w-[87%] h-[30px] bg-slate-200 text-slate-900 text-sm font-medium caret-slate-900 outline-0 px-4 placeholder:text-slate-900 placeholder:text-sm" />
-                              <button className="w-[13%] h-[30px] bg-slate-300" onClick={() => console.log('clicked')}>
+                              <input type="text" placeholder="Search..." onChange={handleChange} className="w-[87%] h-[30px] bg-slate-200 text-slate-900 text-sm font-medium caret-slate-900 outline-0 px-4 placeholder:text-slate-900 placeholder:text-sm" />
+                              <button className="w-[13%] h-[30px] bg-slate-300" onClick={() => navigate(`movies/${searchQuery}`)}>
                                    <svg className="w-full md:h-[1.1rem] h-[1rem]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#001C30">
                                         <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                                    </svg>
