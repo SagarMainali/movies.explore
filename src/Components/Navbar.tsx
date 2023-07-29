4bc5130e2e2de3eba0ff861e4b7cbc4f729d8e1d
Sagar Mainali <sagarmainali78@gmail.com>
2023-07-29 20:55:20 +0545

    navbar dynamic bg according to routes

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,9 +1,11 @@
-import { NavLink, useNavigate } from "react-router-dom"
+import { NavLink, useNavigate, useLocation } from "react-router-dom"
 import '../styles/navbar.css'
 import { useGlobalContext } from '../stateManagement/context'
 import { ChangeEvent, useEffect, useState, useRef } from "react"
 
 export function Navbar() {
+
+     const { pathname } = useLocation()
 
      const { menuTogglerActive, changeMenuTogglerState } = useGlobalContext()
 
@@ -56,7 +58,8 @@ export function Navbar() {
           // max-2xl:w-[calc(100%-24px)]
           <div
                className={`fixed w-full max-w-[1500px] z-50 px-3 duration-300 
-               ${hideNavbar ? '-translate-y-[100%]' : ''} ${crossed300 ? 'bg-primary-dark/80' : 'bg-primary-dark/40'}`}>
+               ${hideNavbar ? '-translate-y-[100%]' : ''} 
+               ${pathname === '/' && !crossed300 ? 'bg-primary-dark/40' : 'bg-primary-dark/80'}`}>
                {/* Logo */}
                <div className="h-[50px] flex justify-between items-center">
 
@@ -66,7 +69,7 @@ export function Navbar() {
 
                     {/* dymanic class - (above sm - dont' translate at all, keep menu where it is) (below sm - translate -100% and upon toggling translate to default i.e 0)*/}
                     <div className={`flex items-center gap-3 menu-main duration-300 md:translate-x-0 -translate-x-[100%] 
-                         ${menuTogglerActive ? 'translate-x-0' : ''}`}>
+                         ${menuTogglerActive ? 'translate-x-0' : ''} `}>
 
                          {/* Routes */}
                          <ul className="flex font-medium gap-1 menu-links">
@@ -85,7 +88,7 @@ export function Navbar() {
                                    onChange={handleChange}
                                    className="w-[87%] h-[30px] bg-slate-200 text-slate-900 text-sm font-medium caret-slate-900 
                                    outline-0 px-4 placeholder:text-slate-900 placeholder:text-sm" />
-                              <button className="w-[13%] h-[30px] bg-slate-300" onClick={() => navigate(`movies/${searchQuery}`)}>
+                              <button className="w-[13%] h-[30px] bg-slate-300" onClick={() => navigate(`movies / ${searchQuery} `)}>
                                    <svg className="w-full md:h-[1.1rem] h-[1rem]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#001C30">
                                         <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                                    </svg>
--- src/Pages/Explorer.tsx
+++ src/Pages/Explorer.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react"
 import { fetchDataFromApi } from "../utils/api"
 import { Loading } from "../Components/helperComponents/Loading"
 import { MovieAndShowsDetails } from "../types/type"
@@ -8,16 +7,12 @@ export function Explorer({ explore }: { explore: string }) {
 
      const { data, isLoading } = fetchDataFromApi(`/discover/${explore}`)
 
-     useEffect(() => {
-          console.log(data)
-     }, [isLoading])
-
      return (
           isLoading
                ?
                <Loading />
                :
-               <div className="pt-[50px] grid grid-cols-5 gap-4">
+               <div className="pt-[60px] grid grid-cols-5 gap-4">
                     {
                          data?.map(
                               (movie: MovieAndShowsDetails) => <Card key={movie.id} {...movie} />
