97362bf49e4acf6f7a30d3acd609d30a94709b8d
Sagar Mainali <sagarmainali78@gmail.com>
2023-07-29 19:48:06 +0545

    added api functionality in movies and tvshows page

--- src/Pages/Movies.tsx
+++ src/Pages/Movies.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react"
+import { fetchDataFromApi } from "../utils/api"
+
 export function Movies() {
+
+     const { data, isLoading } = fetchDataFromApi('/discover/movie')
+
+     useEffect(() => {
+          console.log(data)
+     }, [isLoading])
+
      return (
-          <div className="pt-[50px]">
-               Movies
+          <div className="pt-[50px] flex">
+
           </div>
      )
 }
--- src/Pages/Tvshows.tsx
+++ src/Pages/Tvshows.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react"
+import { fetchDataFromApi } from "../utils/api"
+
 export function Tvshows() {
+
+     const { data, isLoading } = fetchDataFromApi('/discover/tv')
+
+     useEffect(() => {
+          console.log(data)
+     }, [isLoading])
+
      return (
-          <div className="pt-[50px]">
-               Tvshows
+          <div className="pt-[50px] flex">
           </div>
      )
 }
